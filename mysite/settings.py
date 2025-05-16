f9b12231090aaa97190a185189499b99814c66c6
Erishprajapati <irishmjn@gmail.com>
2025-05-16 22:48:16 +0545

    ..1

--- mysite/settings.py
+++ mysite/settings.py
@@ -121,6 +121,9 @@ STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
+# Add this line for Whitenoise
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
